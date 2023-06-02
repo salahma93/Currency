d15c4928af51234f464f8a84c3ad851ebbe65b50
Mohamed Salah <mohamed.salah-younis@vodafone.com>
2023-06-02 16:48:43 +0300

    FF-0: add NetworkServiceProtocol to be implemented by different network services

---
+++ Currency/Network Layer/Service/NetworkServiceProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkServiceProtocol.swift
+//  Currency
+//
+//  Created by Mohamed Salah on 02/06/2023.
+//
+
+import Foundation
+
+protocol NetworkServiceProtocol {
+	func start<ParamsModel: Encodable, ResponseModel: Codable>(with params: ParamsModel, completion: @escaping (Result<ResponseModel, Error>) -> Void)
+	
+	func cancel()
+}
